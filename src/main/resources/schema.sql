46aa1ba6e9c14d9d67fac3f023fd2d38978e9bf0
afshinparhizkari <Afshin.Parhizkari@gmail.com>
2022-01-12 01:03:02 +0330

    database ver1 is completed

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,29 +1,30 @@
 DROP TABLE IF EXISTS developer;
 CREATE TABLE developer (
-    name varchar2(10) IDENTITY PRIMARY KEY
+    devname varchar2(10) NOT NULL PRIMARY KEY
 );
 DROP TABLE IF EXISTS issue;
 CREATE TABLE issue (
-    issueID INT AUTO_INCREMENT IDENTITY PRIMARY KEY,
-    title varchar2(20) NOT NULL,
-    description varchar2(50),
-    creation_date datetime NOT NULL DEFAULT current_timestamp(),
-    assigned_developer varchar2(10) NOT NULL,
-    foreign key (assigned_developer) references developer(name)
+    issueid INT IDENTITY PRIMARY KEY,
+    issuetype INT NOT NULL,/*0=story, 1=bug*/
+    title varchar2(40) NOT NULL,
+    description varchar2(100),
+    creationdate datetime NOT NULL DEFAULT current_timestamp(),
+    assignedev varchar2(10),
+    foreign key (assignedev) references developer(devname)
 );
 DROP TABLE IF EXISTS story;
 CREATE TABLE story (
-    issueID INT AUTO_INCREMENT IDENTITY PRIMARY KEY,
-    estimated_point_value INT NOT NULL,/*from 1(Low) to 5(High)*/
+    issueid INT IDENTITY PRIMARY KEY,
+    estimatedpoint INT NOT NULL,/*from 1(Low) to 5(High)*/
     status varchar2(10) NOT NULL,/*New,Estimated,Completed*/
-    foreign key (issueID) references issue(issueID)
+    foreign key (issueid) references issue(issueid)
 );
 DROP TABLE IF EXISTS bug;
 CREATE TABLE bug (
-    issueID INT AUTO_INCREMENT IDENTITY PRIMARY KEY,
+    issueid INT IDENTITY PRIMARY KEY,
     priority varchar2(10) NOT NULL,/*Critical,Major,Minor*/
     status varchar2(10) NOT NULL,/*New,Verified,Resolved*/
-    foreign key (issueID) references issue(issueID)
+    foreign key (issueid) references issue(issueid)
 );
 
 /*
